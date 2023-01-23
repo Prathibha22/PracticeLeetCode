9cde7eae5ad392080c461da25cc394fce7aa26a5
prathibha22 <prccherukuri@gmail.com>
2023-01-23 09:24:17 -0500

    comments added

--- src/main/java/gameWinnerWendyorBob/gameWinner.java
+++ src/main/java/gameWinnerWendyorBob/gameWinner.java
@@ -5,16 +5,19 @@ public class gameWinner {
         //Input wwwbbbbwww
         //output bob wins
         findTurn turn=findTurn.WENDY;
+        //checking for possibility of removing character
         while(input.contains(turn._checkCharSeq)) {
             int index=input.indexOf(turn._checkCharSeq);
             StringBuilder sb=new StringBuilder(input);
             sb.deleteCharAt(index+1);
             input=sb.toString();
+            //changing the turn
             turn=turn.equals(findTurn.WENDY)?findTurn.BOB:findTurn.WENDY;
         }
         turn=turn.equals(findTurn.WENDY)?findTurn.BOB:findTurn.WENDY; //the winner will be the one with last move
         return turn.toString();
     }
+    //define the players and what to have to win the move
     public enum findTurn {
         WENDY("www") ,
         BOB("bbb");
